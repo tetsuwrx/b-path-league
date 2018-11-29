7df8edb5c2eca160e96226ef14cf82c647df915e
tetsuwrx <tetsuwrx@gmail.com>
2018-11-29 23:44:01 +0900

    Update score.js

--- web/js/score.js
+++ web/js/score.js
@@ -8,3 +8,14 @@ function setClass1()
   document.entryForm.p1Class.value = classVal[2];
   document.entryForm.p1ScoreWin.value = classVal[3];
 }
+
+function setClass2()
+{
+  var p1Name = document.entryForm.p2Name;
+  var selInd = p2Name.selectedIndex;
+  var nameVal = p2Name.options[selInd].value;
+  var classVal = nameVal.split(',');
+
+  document.entryForm.p2Class.value = classVal[2];
+  document.entryForm.p2ScoreWin.value = classVal[3];
+}
