6e017eca22ad18e43efdc2e2bd8a733cf9c254d1
tetsuwrx <tetsuwrx@gmail.com>
2018-11-29 23:31:18 +0900

    Update score.js

--- web/js/score.js
+++ web/js/score.js
@@ -1,6 +1,8 @@
 function setClass1()
 {
-  var nameVal = document.getElementsByName("p1Name").value;
+  var p1Name = document.entryForm.p1Name;
+  var selInd = p1Name.selectedIndex;
+  var nameVal = p1Name.options[selInd].value; 
   var classVal = nameVal.split(',');
 
   document.getElementsByName("p1Class").text = classVal;
