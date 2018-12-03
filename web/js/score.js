61f5702dbee5962fb6027f32fe7be56cc2ee81dd
tetsuwrx <tetsuwrx@gmail.com>
2018-12-03 13:16:10 +0900

    score edit

--- web/js/score.js
+++ web/js/score.js
@@ -3,10 +3,13 @@ function setClass1()
   var p1Name = document.entryForm.p1Name;
   var selInd = p1Name.selectedIndex;
   var nameVal = p1Name.options[selInd].value;
-  var classVal = nameVal.split(',');
+  if ( nameVal != "0" )
+  {
+    var classVal = nameVal.split(',');
 
-  document.entryForm.p1Class.value = classVal[2];
-  document.entryForm.p1ScoreWin.value = classVal[3];
+    document.entryForm.p2Class.value = classVal[2];
+    document.entryForm.p2ScoreWin.value = classVal[3];
+  }
 }
 
 function setClass2()
@@ -14,10 +17,13 @@ function setClass2()
   var p2Name = document.entryForm.p2Name;
   var selInd = p2Name.selectedIndex;
   var nameVal = p2Name.options[selInd].value;
-  var classVal = nameVal.split(',');
+  if ( nameVal != "0" )
+  {
+    var classVal = nameVal.split(',');
 
-  document.entryForm.p2Class.value = classVal[2];
-  document.entryForm.p2ScoreWin.value = classVal[3];
+    document.entryForm.p2Class.value = classVal[2];
+    document.entryForm.p2ScoreWin.value = classVal[3];
+  }
 }
 
 function scoreInput(player)
