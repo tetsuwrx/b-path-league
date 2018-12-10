ff44956a3638c8017d43c4a4a9a3cf6f614c8726
tetsuwrx <tetsuwrx@gmail.com>
2018-12-11 01:01:53 +0900

    Update scorelist.js

--- web/js/scorelist.js
+++ web/js/scorelist.js
@@ -126,3 +126,15 @@ function refineMember(refineVal, selectObj, selectAllObj)
     }
   }
 }
+
+function displayChange( id )
+{
+  var obj = document.getElementById( id );
+
+  if ( obj.style.display == "" )
+  {
+    obj.style.display = "none";
+  }else if ( obj.style.display == "none" ) {
+    obj.style.display = "";
+  }
+}
