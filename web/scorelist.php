ff1388de927020da9f6f87d7ce64907d8fad8a85
tetsuwrx <tetsuwrx@gmail.com>
2018-12-12 00:33:50 +0900

    Update scorelist.php

--- web/scorelist.php
+++ web/scorelist.php
@@ -41,10 +41,18 @@ $prevToDate = date('Y-m-d', strtotime($nowDate . 'last day of previous month'));
 //全試合結果を取得
 $scorelistall = $dtutils->getMatchReport($prevFromDate,$toDate,$memberno);
 $scorelist = $dtutils->aggregateRankingBase($scorelistall);
+$scoreresult = $dtutils->aggregateForReport($scorelist);
 //先月の結果を集計
 $rankinglistPrev = $dtutils->getMatchReport($prevFromDate,$prevToDate,$memberno);
 $rankingbasePrev = $dtutils->aggregateRankingBase($rankinglistPrev);
 // ドロップダウン用のメンバーリスト取得
 $memberlist = $dtutils->getMemberList();
 
-echo $app['twig']->render('scorelist.twig', array('dateFrom' => $prevFromDate, 'dateTo' => $toDate, 'scorelist' => $scorelist, 'memberlist' => $memberlist, 'membername' => $membername) );
+$param = array( 'dateFrom' => $prevFromDate
+              , 'dateTo' => $toDate
+              , 'scorelist' => $scorelist
+              , 'memberlist' => $memberlist
+              , 'membername' => $membername
+              , 'scoreresult' => $scoreresult
+              );
+echo $app['twig']->render('scorelist.twig',  $param);
