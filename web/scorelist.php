cc2ded979fba6c4c0843f27ed459fb5f2a8e02fb
tetsuwrx <tetsuwrx@gmail.com>
2018-12-11 17:15:02 +0900

    scorelist edit

--- web/scorelist.php
+++ web/scorelist.php
@@ -21,14 +21,29 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $dtutils = new DataUtils();
 $score = array();
 
-$dateFrom = $_REQUEST['dateFrom'];
-$dateTo = $_REQUEST['dateTo'];
+// $dateFrom = $_REQUEST['dateFrom'];
+// $dateTo = $_REQUEST['dateTo'];
+
 $memberno = $_REQUEST['p1No'];
 
-$scorelist = $dtutils->getScoreList($dateFrom,$dateTo);
+//現在の月を取得
+$nowMonth = date('Y-n');
+//現在の日付から、月初と月末の日付を取得
+$fromDate = date('Y-m-d', strtodate('first day of ' . $nowMonth));
+$toDate = date('Y-m-d', strtodate('last day of ' . $nowMonth));
+$nowDate = date('Y-m-d');
+//先月の月初と月末の日付を取得
+$prevFromDate = date('Y-m-d', strtodate($nowDate . 'first day of previous month'));
+$prevToDate = date('Y-m-d', strtodate($nowDate . 'last day of previous month'));
 
+//全試合結果を取得
+$scorelistall = $dtutils->getScoreList($prevFromDate,$toDate);
+
+//先月の結果を集計
+$rankinglistPrev = $dtutils->getMatchReport($prevFromDate,$prevToDate,$memberno);
+$rankingbasePrev = $dtutils->aggregateRankingBase($rankinglist);
+
+// ドロップダウン用のメンバーリスト取得
 $memberlist = $dtutils->getMemberList();
-$rankinglist = $dtutils->getMatchReport($dateFrom,$dateTo,$memberno);
-$rankingbase = $dtutils->aggregateRankingBase($rankinglist);
 
-echo $app['twig']->render('scorelist.twig', array('dateFrom' => $dateFrom, 'dateTo' => $dateTo, 'scorelist' => $scorelist, 'memberlist' => $memberlist) );
+echo $app['twig']->render('scorelist.twig', array('dateFrom' => $prevFromDate, 'dateTo' => $toDate, 'scorelist' => $scorelistall, 'memberlist' => $memberlist) );
