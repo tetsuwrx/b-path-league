fec5d4a6b1dd6446e354a3206ebe33eea89b704b
tetsuwrx <tetsuwrx@gmail.com>
2019-01-06 15:38:22 +0900

    Update scoreregist.js

--- web/js/scoreregist.js
+++ web/js/scoreregist.js
@@ -24,3 +24,44 @@ function setClass(nameObj, classObj, masuObj)
     classObj.value = "";
   }
 }
+
+function registScore()
+{
+  if ( checkEntry() == true )
+  {
+    window.alert('登録はっじめーるよー！');
+  }
+}
+
+function checkEntry()
+{
+  for ( i = 1; i < scorelist.rows.length; i++; )
+  {
+    var class1val = document.getElementById('class1_' + i).value;
+    var score1val = document.getElementById('score1_' + i).value;
+
+    var class2val = document.getElementById('class2_' + i).value;
+    var score2val = document.getElementById('score2_' + i).value;
+
+    var errMsg = "";
+
+    if ( !(class1val != null && score1val != null && class2val != null && score2val != null ) )
+    {
+      if ( score1val != null )
+      {
+        errMsg += i + "行目:スコア1が未入力です\r\n";
+      }else if ( score2val != null)
+      {
+        errMsg += i + "行目:スコア2が未入力です\r\n";
+      }
+    }
+  }
+  if ( errMsg != "" )
+  {
+    window.alert(errMsg);
+    return false;
+  }else {
+    return true;
+  }
+
+}
