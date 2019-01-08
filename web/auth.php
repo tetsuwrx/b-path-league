00b1962a0fa490c45ee29fdf1e79a31d012e3774
tetsuwrx <tetsuwrx@gmail.com>
2019-01-08 13:30:29 +0900

    Update auth.php

--- web/auth.php
+++ web/auth.php
@@ -3,7 +3,7 @@ $password = $_REQUEST['password'];
 
 if ( $password == '8804' )
 {
-  header('https://b-path-league.herokuapp.com/maintenance', true, 301);
+  header('Location:/maintenance', true, 301);
 }
 echo '<p>パスワードが間違ってます</p>';
 
