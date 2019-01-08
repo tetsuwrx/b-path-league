bb64fdb102a7a2dc0c79dc8a0987b437779ed014
tetsuwrx <tetsuwrx@gmail.com>
2019-01-08 13:27:46 +0900

    Update auth.php

--- web/auth.php
+++ web/auth.php
@@ -7,9 +7,4 @@ if ( $password == '8804' )
 }
 echo '<p>パスワードが間違ってます</p>';
 
-<form name="entryForm" method="POST" target="_self" action="auth.php">
-<p>パスワード？<br>
-<input type="password" name="password" value=""></p>
-<input class="a1btns" type="submit" name="a1submit" value="送信">
-</form>
 ?>
