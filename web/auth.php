811e7a2b82ba5fa302a0a29105ea178bc44b1d32
tetsuwrx <tetsuwrx@gmail.com>
2019-01-08 17:06:46 +0900

    Update auth.php

--- web/auth.php
+++ web/auth.php
@@ -3,8 +3,20 @@ $password = $_REQUEST['password'];
 
 if ( $password == '8804' )
 {
+  session_start();
+
+  $_SESSION['auth'] = 'true';
+  
   header('Location:/maintenance', true, 301);
+}else{
+  echo '<p>パスワードが間違ってます</p>';
+
+  // セッションの値を初期化
+  $_SESSION = array();
+
+  // セッションを破棄
+  session_destroy();
 }
-echo '<p>パスワードが間違ってます</p>';
+
 
 ?>
