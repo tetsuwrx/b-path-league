ad52b00a2295e99443b07bcdf5081bb1f4fae3ff
tetsuwrx <tetsuwrx@gmail.com>
2019-01-09 17:28:28 +0900

    Update bowlard.js

--- web/js/bowlard.js
+++ web/js/bowlard.js
@@ -39,3 +39,41 @@ function getEmptyCell()
     }
   }
 }
+
+function getEmptyTd()
+{
+  for ( var i = 1; i <= 10; i++ ){
+    var idkey1 = 'frame' + i + '-1';
+    var idkey2 = 'frame' + i + '-2';
+
+    var tdkey1 = 'td' + i + '-1';
+    var tdkey2 = 'td' + i + '-2';
+
+    var frameObj = document.getElementById(idkey1);
+    if ( frameObj.innerText == "" )
+    {
+      var tdObj = document.getElementById(tdkey1);
+      return tdObj;
+    }
+
+    var frameObj = document.getElementById(idkey2);
+    if ( frameObj.innerText == "" )
+    {
+      var tdObj = document.getElementById(tdkey2);
+      return tdObj;
+    }
+
+    if ( i == 10 )
+    {
+      var idkey2 = 'frame10-3';
+      var tdkey3 = 'td10-3';
+
+      var frameObj = document.getElementById(idkey3);
+      if ( frameObj.innerText == "" )
+      {
+        var tdObj = document.getElementById(tdkey3);
+        return tdObj;
+      }
+    }
+  }
+}
